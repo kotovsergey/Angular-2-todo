8135dc627adf42af85fcc7c49b5b8a730b95b10b
Sergey <neuclear95@gmail.com>
2016-11-27 18:41:13 +0300

    angular 2 app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,7 @@
 import {Component} from '@angular/core';
 
-import { TodoService } from './shared/todo.service';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
-import {Todo} from './shared/todo.model';
-import {TodoFormComponent} from './components/todo-form/todo-form.component';
-import { todos } from './shared/todo.data'
-
 @Component({
-    selector: 'todo-app',
-    templateUrl: './app/app.component.html',
-    styleUrls: ['./app/app.component.css'],
-    directives: [TodoListComponent, TodoFormComponent],
-    providers: [TodoService]
+    selector: 'my-app',
+    template: '<h1>Angular 2</h1>'
 })
-export class AppComponent {
-  title: string;
-
-  constructor(){
-    this.title = 'Angular 2Do';
-  }
-
-}
+export class AppComponent { }
--- app/shared/todo.data.ts
+++
@@ -1,8 +0,0 @@
-import {ITodo} from './todo.model'
-
-export let todos = [
-  { title: 'Learn TypeScript', done: false },
-  { title: 'Learn ES6', done: false },
-  { title: 'Learn Angular 2', done: false },
-  { title: 'Rest', done: false }
-];
